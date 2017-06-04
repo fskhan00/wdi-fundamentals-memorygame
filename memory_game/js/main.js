bc79046550dd29df753b51dddf715b0bdc8af829
Sana Khan <Sana@Sanas-MBP.home>
2017-06-04 19:54:44 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,20 @@
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1]);
-
-if(cardsInPlay.length === 2){
-	if(cardsInPlay[0]===cardsInPlay[1]){
+var checkForMatch = function(){
+		if(cardsInPlay[0]===cardsInPlay[1]){
 		alert("You found a match!");
 	}
 	else{
 		alert("Sorry, Try again!");
 	}
+};
 
-}
+var flipCard=function(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if(cardsInPlay.length === 2){
+	checkForMatch();
+	}
+};
+flipCard(0);
+flipCard(2);
